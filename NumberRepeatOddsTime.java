570ec87f6b58149baa27b0f864d1ed1426eb6ca4
barbie6676 <mian@lexity.com>
2013-10-19 13:02:28 -0700

    Create NumberRepeatOddsTime.java

---
+++ NumberRepeatOddsTime.java
@@ -0,0 +1,36 @@
+//in array, every number repeat even times but one, return that number
+
+public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = A.length;
+        if (n == 0) return -1;
+        
+        int ret = 0;
+        for (int i= 0 ;i < n ; i ++){
+            ret ^= A[i];
+        }
+        return ret^0;
+    }
+
+//in array, every number repeat three times but one, return that number  
+
+public int singleNumber(int[] A) {
+    // Note: The Solution object is instantiated only once and is reused by each test case.
+    int n = A.length;
+    if(n == 0){
+        return -1;
+    }
+     
+     int[] counts = new int[32];
+     int result = 0;
+     for(int i = 0; i < 32; i++){
+        for(int j = 0; j < n; j++){
+             if(((A[j] >> i) & 1) == 1){
+                 counts[i] = (counts[i] + 1) % 3;
+             }
+         }
+         result |= (counts[i] << i);
+     }
+     
+    return result;
+}
